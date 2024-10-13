082cd6f8d80b2d6b190cde42e00de25d0fe413ab
Radixactivetea <115629911+Radixactivetea@users.noreply.github.com>
2024-10-13 18:34:10 +0800

    Adding 404 page

--- src/pages/404.php
+++ src/pages/404.php
@@ -1,11 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Star Light Pottery Shop</title>
+
+    <link rel="stylesheet" href="./public/css/404.css">
 </head>
+
 <body>
-    Error 404
+    <div id="main">
+        <div class="fof">
+            <h1>Error 404</h1>
+            <h2>Looks like you've found the doorway to the great nothing</h2>
+            <p>Sorry about that! Please visit our hompage to get where you need to go.</p>
+            <br>
+            <a href="/" class="button"><span>Back to home</span></a>
+        </div>
+    </div>
 </body>
+
 </html>
