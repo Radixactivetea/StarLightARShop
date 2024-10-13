26df74ee8f8340ef0cff16fbaec2003c7c84e00a
Radixactivetea <115629911+Radixactivetea@users.noreply.github.com>
2024-10-14 05:45:39 +0800

    create sortby dropdown

--- src/pages/shop.php
+++ src/pages/shop.php
@@ -17,8 +17,38 @@
     <?php require "./src/components/nav.html"; ?>
 
     <div class="main-grid container">
-        <div class="sort-bar">Sort By</div>
-        <div class="filter-bar">2</div>
+        <div class="sort-bar">
+            <div class="btn-group">
+                <button type="button" class="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown"
+                    aria-expanded="false">
+                    Sort By
+                </button>
+                <ul class="dropdown-menu dropdown-menu-end">
+                    <li><button class="dropdown-item" type="button">Newest</button></li>
+                    <li><button class="dropdown-item" type="button">Price: High - Low</button></li>
+                    <li><button class="dropdown-item" type="button">Price: Low - High</button></li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="filter-bar">
+            <div class="accordion accordion-flush" id="accordionFlushExample">
+                <div class="accordion-item">
+                    <h2 class="accordion-header">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                            Accordion Item #1
+                        </button>
+                    </h2>
+                    <div id="flush-collapseOne" class="accordion-collapse collapse"
+                        data-bs-parent="#accordionFlushExample">
+                        <div class="accordion-body">Placeholder content for this accordion, which is intended to
+                            demonstrate the <code>.accordion-flush</code> class. This is the first item's accordion
+                            body.</div>
+                    </div>
+                </div>
+            </div>
+        </div>
         <div class="shop-content">3</div>
     </div>
 
