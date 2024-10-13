e30e7257276bfe3963711df9b1b900db1c4a481d
Radixactivetea <115629911+Radixactivetea@users.noreply.github.com>
2024-10-13 14:12:21 +0800

    initialize main

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+$request = $_SERVER['REQUEST_URI'];
+$viewDir = '/src/pages/';
+
+switch ($request) {
+    case '':
+    case '/':
+    case '/home':
+        require __DIR__ . $viewDir . 'home.php';
+        break;
+
+    case '/shop':
+        require __DIR__ . $viewDir . 'shop.php';
+        break;
+
+    default:
+        http_response_code(404);
+        require __DIR__ . $viewDir . '404.php';
+}
---
+++ src/pages/home.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>StarLightARShop</title>
+    <!-- bootstap css & override -->
+    <link rel="stylesheet" href="./node_modules/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./public/css/bs-theme-overrides.css">
+    <!-- shop style -->
+    
+</head>
+
+<body>
+    <!-- navigator -->
+     <?php require "./src/components/nav.html"; ?>
+
+
+
+
+
+
+
+
+
+
+
+     
+    <!-- footer -->
+    <?php require "./src/components/footer.html"; ?>
+
+    <!-- bootstap and popper -->
+    <script src="./node_modules/@popperjs/core/dist/umd/popper.min.js"></script>
+    <script src="./node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+</body>
+
+</html>
