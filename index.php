53912b0550d295d9b0e8fcd3d3fbb3d41e4ac63e
Radixactivetea <115629911+Radixactivetea@users.noreply.github.com>
2024-10-13 15:57:47 +0800

    upgrade router
    use simple router library

--- index.php
+++ index.php
@@ -1,19 +1,3 @@
 <?php
 
-$request = $_SERVER['REQUEST_URI'];
-$viewDir = '/src/pages/';
-
-switch ($request) {
-    case '':
-    case '/':
-        require __DIR__ . $viewDir . 'home.php';
-        break;
-
-    case '/shop':
-        require __DIR__ . $viewDir . 'shop.php';
-        break;
-
-    default:
-        http_response_code(404);
-        require __DIR__ . $viewDir . '404.php';
-}
+require 'router/routes.php';
