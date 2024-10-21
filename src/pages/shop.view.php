3fce4346dbe44a915eb28a7cba9a915b1997a790
Radixactivetea <115629911+Radixactivetea@users.noreply.github.com>
2024-10-22 01:04:59 +0800

    restart shop-page

--- src/pages/shop.view.php
+++ src/pages/shop.view.php
@@ -17,46 +17,11 @@
     <?php require "./src/components/nav.php"; ?>
 
     <div class="main-grid container">
-        <div class="sort-bar">
-            <div class="btn-group">
-                <button type="button" class="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown"
-                    aria-expanded="false">
-                    Sort By
-                </button>
-                <ul class="dropdown-menu dropdown-menu-end">
-                    <li><button class="dropdown-item" type="button">Newest</button></li>
-                    <li><button class="dropdown-item" type="button">Price: High - Low</button></li>
-                    <li><button class="dropdown-item" type="button">Price: Low - High</button></li>
-                </ul>
-            </div>
-        </div>
-
         <div class="filter-bar">
-            <div class="accordion accordion-flush" id="filterby-accordion">
-                <div class="accordion-item">
-                    <h2 class="accordion-header">
-                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                            data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
-                            Filter By
-                        </button>
-                    </h2>
-                    <div id="flush-collapseOne" class="accordion-collapse collapse"
-                        data-bs-parent="#filterby-accordion">
-                        <div class="accordion-body">
-                            <ul class="list-group">
-                                <li class="list-group-item">Oil Dispenser </li>
-                                <li class="list-group-item">Bowl</li>
-                                <li class="list-group-item">Mug</li>
-                                <li class="list-group-item">Plate</li>
-                                <li class="list-group-item">Vase</li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            
         </div>
         <div class="shop-content">
-            
+
         </div>
     </div>
 
