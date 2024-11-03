fa1fc9d4b10265e282bf3b4befba52c6d459a1e9
Radixactivetea <115629911+Radixactivetea@users.noreply.github.com>
2024-11-04 02:16:35 +0800

    Update product.view.php
    finist the layout of product page

--- src/pages/product.view.php
+++ src/pages/product.view.php
@@ -19,10 +19,10 @@
 
     <!-- Product section-->
     <section class="py-5">
-        <div class="container px-4 px-lg-5 my-5">
+        <div class="container my-5">
             <div class="row gx-4 gx-lg-5 align-items-start">
                 <div class="col-md-6"><img class="card-img-top mb-5 mb-md-0"
-                        src="https://dummyimage.com/600x700/dee2e6/6c757d.jpg" alt="..." /></div>
+                        src="https://tailwindui.com/plus/img/ecommerce-images/category-page-04-image-card-01.jpg" alt="Product Item" /></div>
                 <div class="col-md-6">
                     <h2 class="fw-bolder">Shop item template</h2>
                     <div class="mb-5">
@@ -98,114 +98,64 @@
             </div>
         </div>
     </section>
+
+    <!-- Review section -->
+    <section class="py-5 d-none">
+        <div class="container">
+            <h4 class="text-center mb-4">People Love Us</h4>
+            <div class="row border-top border-bottom border-primary-subtle py-4">
+                <div class="col-12 col-lg-10">
+                    <h6 class="mb-0">Reviews</h6>
+                </div>
+                <div class="col-12 col-lg-2 d-none d-lg-block">
+                    <h6 class="mb-0">Rating</h6>
+                </div>
+            </div>
+            <div class="row py-3 border-bottom border-primary-subtle">
+                <div class="col-12 col-lg-10">
+                    <div class="d-flex flex-column flex-sm-row gap-3">
+                        <img src="https://pagedone.io/asset/uploads/1704364459.png" alt="Profile Image"
+                            class="rounded-circle object-fit-cover" style="width: 8rem; height: 8rem;">
+                        <div>
+                            <p class="fw-medium fs-5 mb-2 text-dark">Robert Karmazov</p>
+                            <div class="d-flex d-lg-none align-items-center gap-2 mb-4">
+                                <!-- for icon review -->
+                            </div>
+                            <p class="fw-normal fs-6 text-secondary mb-4">Lorem ipsum dolor sit amet consectetur
+                                adipisicing elit. Quisquam, ea rerum natus exercitationem molestias laborum fuga
+                                veritatis sint, quaerat sunt fugiat corporis rem quo. Assumenda doloribus cumque atque
+                                mollitia qui.</p>
+                            <p class="fw-medium fs-6 text-secondary mb-0">Nov 01, 2023</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-lg-2 d-none d-lg-block">
+                    <div class="d-flex align-items-center gap-2 mb-4">
+                        <!-- for icon review -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
     <!-- Related items section-->
     <section class="py-5">
-        <div class="container px-4 px-lg-5 mt-5">
-            <h2 class="fw-bolder mb-4">Related products</h2>
-            <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
-                <div class="col mb-5">
-                    <div class="card h-100">
-                        <!-- Product image-->
-                        <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
-                        <!-- Product details-->
-                        <div class="card-body p-4">
-                            <div class="text-center">
-                                <!-- Product name-->
-                                <h5 class="fw-bolder">Fancy Product</h5>
-                                <!-- Product price-->
-                                $40.00 - $80.00
+        <div class="container">
+            <h4 class="mb-4">Related products</h4>
+            <div class="col">
+                <div class="row row-cols-2 row-cols-md-3 row-cols-lg-4 g-3">
+                    <a href="/shop/1" class="col text-decoration-none">
+                        <div class="card h-100 shadow-sm overflow-hidden rounded-lg">
+                            <img src="https://tailwindui.com/plus/img/ecommerce-images/category-page-04-image-card-01.jpg"
+                                alt="Tall slender porcelain bottle with natural clay textured body and cork stopper."
+                                class="card-img-top"   />
+                            <div class="card-body">
+                                <h5 class="card-title text-sm text-gray-700">Earthen Bottle</h5>
+                                <p class="card-text text-lg font-weight-bold text-gray-900">$48</p>
                             </div>
                         </div>
-                        <!-- Product actions-->
-                        <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                            <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">View options</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col mb-5">
-                    <div class="card h-100">
-                        <!-- Sale badge-->
-                        <div class="badge bg-dark text-white position-absolute" style="top: 0.5rem; right: 0.5rem">Sale
-                        </div>
-                        <!-- Product image-->
-                        <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
-                        <!-- Product details-->
-                        <div class="card-body p-4">
-                            <div class="text-center">
-                                <!-- Product name-->
-                                <h5 class="fw-bolder">Special Item</h5>
-                                <!-- Product reviews-->
-                                <div class="d-flex justify-content-center small text-warning mb-2">
-                                    <div class="bi-star-fill"></div>
-                                    <div class="bi-star-fill"></div>
-                                    <div class="bi-star-fill"></div>
-                                    <div class="bi-star-fill"></div>
-                                    <div class="bi-star-fill"></div>
-                                </div>
-                                <!-- Product price-->
-                                <span class="text-muted text-decoration-line-through">$20.00</span>
-                                $18.00
-                            </div>
-                        </div>
-                        <!-- Product actions-->
-                        <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                            <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">Add to cart</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col mb-5">
-                    <div class="card h-100">
-                        <!-- Sale badge-->
-                        <div class="badge bg-dark text-white position-absolute" style="top: 0.5rem; right: 0.5rem">Sale
-                        </div>
-                        <!-- Product image-->
-                        <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
-                        <!-- Product details-->
-                        <div class="card-body p-4">
-                            <div class="text-center">
-                                <!-- Product name-->
-                                <h5 class="fw-bolder">Sale Item</h5>
-                                <!-- Product price-->
-                                <span class="text-muted text-decoration-line-through">$50.00</span>
-                                $25.00
-                            </div>
-                        </div>
-                        <!-- Product actions-->
-                        <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                            <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">Add to cart</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col mb-5">
-                    <div class="card h-100">
-                        <!-- Product image-->
-                        <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
-                        <!-- Product details-->
-                        <div class="card-body p-4">
-                            <div class="text-center">
-                                <!-- Product name-->
-                                <h5 class="fw-bolder">Popular Item</h5>
-                                <!-- Product reviews-->
-                                <div class="d-flex justify-content-center small text-warning mb-2">
-                                    <div class="bi-star-fill"></div>
-                                    <div class="bi-star-fill"></div>
-                                    <div class="bi-star-fill"></div>
-                                    <div class="bi-star-fill"></div>
-                                    <div class="bi-star-fill"></div>
-                                </div>
-                                <!-- Product price-->
-                                $40.00
-                            </div>
-                        </div>
-                        <!-- Product actions-->
-                        <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                            <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">Add to cart</a>
-                            </div>
-                        </div>
-                    </div>
+                    </a>
+                    <!-- more related product (limit to 5) -->
                 </div>
             </div>
         </div>
