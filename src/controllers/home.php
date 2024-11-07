fbb4967b536b2814cd7d70c37b2be70804dd287b
Radixactivetea <115629911+Radixactivetea@users.noreply.github.com>
2024-11-07 16:27:34 +0800

    Revert "Update home.php"
    This reverts commit d117d04442e3dc36256b6a76002f3d1f14d8d35e.

--- src/controllers/home.php
+++ src/controllers/home.php
@@ -1,9 +1,4 @@
 <?php
 
 
-$config = require ('config.php');
-$db = new Database($config['database']);
-
-$promotion = $db->query('SELECT * FROM promotion')->fetchAll();
-
 require 'src/pages/home.view.php';
