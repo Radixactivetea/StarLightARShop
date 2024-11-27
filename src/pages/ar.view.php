2bc742dd5a683ced38c05ab88a8a58a29605b167
Radixactivetea <115629911+Radixactivetea@users.noreply.github.com>
2024-11-28 05:17:35 +0800

    change QR

--- src/pages/ar.view.php
+++ src/pages/ar.view.php
@@ -54,7 +54,7 @@
 
         <div class="text-center my-4">
             <h3>Scan the QR code to open on your phone</h3>
-            <img src="/public/img/<?= htmlspecialchars($getAr['img_url']) ?>" alt="QR Code" width="200" height="200">
+            <img src="/public/upload/qr/<?= htmlspecialchars($getAr['img_url']) ?>" alt="QR Code" width="200" height="200">
             <h2>Or</h2>
             <a href="<?= htmlspecialchars($getAr['qr_link']) ?>"
                 class="link-primary link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover"><?= htmlspecialchars($getAr['qr_link']) ?></a>
