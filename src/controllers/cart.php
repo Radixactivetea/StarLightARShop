3ef0c9ef422873ca1d63d9ca886bf3e29b0a7f52
Radixactivetea <115629911+Radixactivetea@users.noreply.github.com>
2025-01-12 23:15:48 +0800

    cart

--- src/controllers/cart.php
+++ src/controllers/cart.php
@@ -1,4 +1,6 @@
 <?php
 
 
+
+
 require 'src/pages/cart.view.php';
--- src/pages/cart.view.php
+++ src/pages/cart.view.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Star Light Pottery Shop</title>
+    <title>Your Cart</title>
     <link rel="icon" href="/public/img/logo.png" type="image/x-icon">
     <!-- bootstap css & override -->
     <link rel="stylesheet" href="/node_modules/bootstrap/dist/css/bootstrap.min.css">
